6b31b6ed2a6a75d98e62a836a4da4852e67b1be5
jatinrev <jatin@revinfotech.com>
2016-10-12 17:29:06 +0530

    6. Routing - /heroes route working, '/' is not working.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -10,7 +12,13 @@ import { HeroService } from './hero.service';
 @NgModule({
     imports:      [ 
         BrowserModule,
-        FormsModule 
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            }
+        ])
     ],
     declarations: [
         AppComponent,
